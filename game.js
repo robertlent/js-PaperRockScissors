6148fe65a09b42135afbde3371f2644f53fd646e
Robert <robertmlent@users.noreply.github.com>
2015-05-23 15:23:41 -0700

    Update game.js

--- game.js
+++ game.js
@@ -1,3 +1,14 @@
+function new_game(){
+	var start = prompt("Do you want to start a new game of Paper, Rock, Scissors?").toLowerCase();
+	if (start === "yes"){
+		game();
+	} else if (start === "no"){
+		sweetAlert("Maybe another time, then. Please click the 'Play' button if you change your mind. Goodbye!");
+	} else {
+		sweetAlert("You did not answer 'Yes' or 'No'. If you would like to play, please click the 'Play' button again and answer 'yes'.");
+	}
+}
+
 function game(){
     var userChoice = prompt("Do you choose Rock, Paper or Scissors?").toLowerCase();
     var computerChoice = Math.random();
@@ -10,26 +21,26 @@ function game(){
     };
 
     if (userChoice === computerChoice) {
-        alert("You both chose " + userChoice + ". This game is a tie!");
+        sweetAlert("You both chose " + userChoice + ". This game is a tie!");
     } else if (userChoice === "rock") {
         if (computerChoice === "scissors") {
-            alert("You chose: " + userChoice + ".\nThe computer chose: " + computerChoice + "\n\nRock crushes Scissors. You win!");
+            sweetAlert("You chose: " + userChoice + ".\nThe computer chose: " + computerChoice + "\n\nRock crushes Scissors. You win!");
         } else {
-            alert("You chose: " + userChoice + ".\nThe computer chose: " + computerChoice + "\n\nPaper covers Rock. You lose!");
+            sweetAlert("You chose: " + userChoice + ".\nThe computer chose: " + computerChoice + "\n\nPaper covers Rock. You lose!");
         }
     } else if (userChoice === "paper"){
         if (computerChoice === "rock"){
-            alert("You chose: " + userChoice + ".\nThe computer chose: " + computerChoice + "\n\nPaper covers Rock. You win!");
+            sweetAlert("You chose: " + userChoice + ".\nThe computer chose: " + computerChoice + "\n\nPaper covers Rock. You win!");
         } else {
-            alert("You chose: " + userChoice + ".\nThe computer chose: " + computerChoice + "\n\nScissors cut Paper. You lose!");
+            sweetAlert("You chose: " + userChoice + ".\nThe computer chose: " + computerChoice + "\n\nScissors cut Paper. You lose!");
         }
     } else if (userChoice === "scissors"){
         if (computerChoice === "rock"){
-            alert("You chose: " + userChoice + ".\nThe computer chose: " + computerChoice + "\n\nRock crushes Scissors. You lose!");
+            sweetAlert("You chose: " + userChoice + ".\nThe computer chose: " + computerChoice + "\n\nRock crushes Scissors. You lose!");
         } else {
-            alert("You chose: " + userChoice + ".\nThe computer chose: " + computerChoice + "\n\nScissors cut Paper. You win!");
+            sweetAlert("You chose: " + userChoice + ".\nThe computer chose: " + computerChoice + "\n\nScissors cut Paper. You win!");
         }
     } else {
-    	alert("Since you did not choose Paper, Rock, or Scissors, you lose!");
+    	sweetAlert("Since you did not choose Paper, Rock, or Scissors, you lose!");
     }	
     }
